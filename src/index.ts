955a34769f01e2d5affa02c324c2a9669890f55f
powdevlin12 <thudatdl123@gmail.com>
2023-10-03 23:51:03 +0700

    init server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-const name: string = 'Dư Thanh Được'
-console.log(name)
+import express, { Request, Response } from 'express'
+const app = express()
+const port = 3000
+
+// Định nghĩa các route và xử lý các yêu cầu
+app.get('/', (req: Request, res: Response) => {
+  res.send('Chào mừng đến với Express.js server!')
+})
+
+// Khởi động server
+app.listen(port, () => {
+  console.log(`Server đang lắng nghe ở cổng ${port}`)
+})
